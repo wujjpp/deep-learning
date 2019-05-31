6d4e2a7b04861dbde6805fc4ea481a261b730136
Wu Jian Ping <wujjpp@hotmail.com>
2019-05-31 22:45:58 +0800

    .

--- high-level-apis/mnist-keras-mylayer.py
+++ high-level-apis/mnist-keras-mylayer.py
@@ -1,45 +1,60 @@
 import tensorflow as tf
+
 mnist = tf.keras.datasets.mnist
 
 
-class MyModel(tf.keras.Model):
-    def __init__(self, input_shape=(28, 28), num_classes=10):
-        super(MyModel, self).__init__(name='my_model')
+class MyLayer(tf.keras.layers.Layer):
+    def __init__(self, output_dim, **kwargs):
+        self.output_dim = output_dim
+        super(MyLayer, self).__init__(**kwargs)
 
-        self.num_classes = num_classes
+    def build(self, input_shape):
+        shape = tf.TensorShape((input_shape[1], self.output_dim))
+        # Create a trainable weight variable for this layer.
 
-        self.flatten = tf.keras.layers.Flatten(input_shape=input_shape)
-        self.dense_1 = tf.keras.layers.Dense(
-            512, activation=tf.keras.activations.relu)
-        self.dense_2 = tf.keras.layers.Dropout(0.2)
-        self.dense_3 = tf.keras.layers.Dense(
-            num_classes, activation=tf.keras.activations.softmax)
+        self.kernel = self.add_weight(name='kernel',
+                                      shape=shape,
+                                      initializer='uniform',
+                                      trainable=True)
+
+        bias_shape = tf.TensorShape((self.output_dim))
+        self.bias = self.add_weight(name='bias',
+                                    shape=bias_shape,
+                                    initializer='uniform',
+                                    trainable=True)
+
+        # Be sure to call this at the end
+        super(MyLayer, self).build(input_shape)
 
     def call(self, inputs):
-
-        # Define your forward pass here,
-        # using layers you previously defined (in `__init__`).
-        # self.layers -> tf.keras.Model -> tf.pyhton.keras.engine.Network
-        x = self.layers[0](inputs)
-        for layer in self.layers[1:]:
-            x = layer(x)
-        return x
+        # return tf.matmul(inputs, self.kernel)
+        return tf.matmul(inputs, self.kernel) + self.bias
 
     def compute_output_shape(self, input_shape):
-        # You need to override this function if you want to use the subclassed model
-        # as part of a functional-style model.
-        # Otherwise, this method is optional.
         shape = tf.TensorShape(input_shape).as_list()
-        shape[-1] = self.num_classes
+        shape[-1] = self.output_dim
         return tf.TensorShape(shape)
+
+    def get_config(self):
+        base_config = super(MyLayer, self).get_config()
+        base_config['output_dim'] = self.output_dim
+        return base_config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
 
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 
 def create_model():
-    model = MyModel()
+    model = tf.keras.models.Sequential()
+
+    model.add(tf.keras.layers.Flatten(name='flatten_1'))
+    model.add(MyLayer(10, name='custom_layer_1'))
 
     model.compile(optimizer=tf.keras.optimizers.Adam(),
                   loss=tf.keras.losses.sparse_categorical_crossentropy,
@@ -50,7 +65,9 @@ def create_model():
 
 def main():
     model = create_model()
-    model.fit(x_train, y_train, epochs=5)
+    model.fit(x_train, y_train, epochs=1, callbacks=[
+        tf.keras.callbacks.TensorBoard(log_dir='results')
+    ])
     model.evaluate(x_test, y_test)
 
 
